58133432478f2ad23da08e3e7f8c1c4aa46ef358
Vinay Singh Tomar <tomarvinaysingh70@gmail.com>
2026-08-25 23:56:42 +0530

    setup express server with MongoDB connection and user routes

--- authpractice/index.js
+++ authpractice/index.js
@@ -2,8 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config()
 
+import mongodbConnected from "./config/db.js"
+import userRouter from "./routes/user.js"
+
 
 const app = express();
 const Port = process.env.PORT 
+
+mongodbConnected();
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use("/api/user", userRouter)
 
 app.listen(Port, ()=>(console.log(`server is running on ${Port}`)));

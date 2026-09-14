3282c7ea06b95df3f416ea053ce30f59d23a7011
Vinay Singh Tomar <tomarvinaysingh70@gmail.com>
2026-09-14 20:41:47 +0530

    add cookie-parser middleware for handling cookies

--- authpractice/index.js
+++ authpractice/index.js
@@ -4,6 +4,7 @@ dotenv.config()
 
 import mongodbConnected from "./config/db.js"
 import userRouter from "./routes/user.js"
+import cookieParser from "cookie-parser";
 
 
 const app = express();
@@ -14,6 +15,7 @@ mongodbConnected();
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
 
 app.use("/api/v1/auth", userRouter)
 
